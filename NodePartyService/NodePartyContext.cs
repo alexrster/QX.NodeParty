6a55d31b61d36eb8ab575884fdfaff88e2f75bbd
AY <a@qx.zone>
2016-12-05 18:45:44 +0200

    Commit progress

---
+++ NodePartyService/NodePartyContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Fabric;
+using QX.NodeParty.Composition.Bootstrap;
+
+namespace QX.NodeParty.Host.ServiceFabric
+{
+  internal sealed class NodePartyContext : ServiceContext, INodeRuntime
+  {
+    public NodePartyContext(NodeContext nodeContext, ICodePackageActivationContext codePackageActivationContext,
+      string serviceTypeName, Uri serviceName, byte[] initializationData, Guid partitionId, long replicaOrInstanceId)
+      : base(nodeContext, codePackageActivationContext, serviceTypeName, serviceName, initializationData, partitionId, replicaOrInstanceId)
+    { }
+
+    public void Dispose()
+    {
+      throw new NotImplementedException();
+    }
+
+    public object CreateObject(string objectFactoryId)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
